74410efd742b2d3688d8a713a72008f9e8bd50e2
Surya Wangsa <suryawangsa5684@gmail.com>
2026-05-15 03:34:16 +0800

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-g2sVyKjM.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/CV/surya-shine-folio-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-Cpfhhalc.js"] }, "/": { filePath: "D:/CV/surya-shine-folio-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CDP-xQRX.js"] } }, clientEntry: "/assets/index-Cpfhhalc.js" });
+export {
+  tsrStartManifest
+};
